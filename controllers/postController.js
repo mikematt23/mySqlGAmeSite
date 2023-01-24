672ed56e56024e009f49b01e525bed1aac30ac3d
Michael MAtteis <michaelcmatteis@gmail.com>
2023-01-23 20:19:29 -0600

    seeing user commeting out the rest of the log in function

--- controllers/postController.js
+++ controllers/postController.js
@@ -32,24 +32,24 @@ async function logIn (req,res){
   const name = req.body.userName
   const password = req.body.password
   const user = await userActions.logIn(name)
-  console.log(user[0].length)
-  if(user[0].length === 0){
-    return res.render('log-in',{m : "User name or password is not correct"})
-  }
-  if(user){
-    const hash = user[0][0].password
-    const isUser = await bcrypt.compare(password,hash);
-    if(!isUser){
-       res.render('404',{m: "Please check your information and try again"})
-    }
+  console.log(user[0])
+  // if(user[0].length === 0){
+  //   return res.render('log-in',{m : "User name or password is not correct"})
+  // }
+  // if(user){
+  //   const hash = user[0][0].password
+  //   const isUser = await bcrypt.compare(password,hash);
+  //   if(!isUser){
+  //      res.render('404',{m: "Please check your information and try again"})
+  //   }
 
-    res.locals.isAuth = true
-    req.session.user = user[0][0]
-    req.session.save(function(){
-      res.render('user',{user: user[0][0]})
-    })
+  //   res.locals.isAuth = true
+  //   req.session.user = user[0][0]
+  //   req.session.save(function(){
+  //     res.render('user',{user: user[0][0]})
+  //   })
    
-  }
+  // }
 }
 
 module.exports = {
