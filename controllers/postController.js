169cc83a2f891b9d3953f3a9f32aeeff68bf44de
Michael MAtteis <michaelcmatteis@gmail.com>
2023-01-23 20:23:56 -0600

    testing

--- controllers/postController.js
+++ controllers/postController.js
@@ -33,23 +33,23 @@ async function logIn (req,res){
   const password = req.body.password
   const user = await userActions.logIn(name)
   console.log(user[0])
-  // if(user[0].length === 0){
-  //   return res.render('log-in',{m : "User name or password is not correct"})
-  // }
-  // if(user){
-  //   const hash = user[0][0].password
-  //   const isUser = await bcrypt.compare(password,hash);
-  //   if(!isUser){
-  //      res.render('404',{m: "Please check your information and try again"})
-  //   }
+  if(user[0].length === 0){
+    return res.render('log-in',{m : "User name or password is not correct"})
+  }
+  if(user[0].length === 1){
+    const hash = user[0].password
+    const isUser = await bcrypt.compare(password,hash);
+    if(!isUser){
+       res.render('404',{m: "Please check your information and try again"})
+    }
 
-  //   res.locals.isAuth = true
-  //   req.session.user = user[0][0]
-  //   req.session.save(function(){
-  //     res.render('user',{user: user[0][0]})
-  //   })
+    res.locals.isAuth = true
+    req.session.user = user[0][0]
+    req.session.save(function(){
+      res.render('user',{user: user[0][0]})
+    })
    
-  // }
+  }
 }
 
 module.exports = {
