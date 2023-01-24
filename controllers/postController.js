aef55ff788d2fe288bafcdda61b37ba3c4150176
Michael MAtteis <michaelcmatteis@gmail.com>
2023-01-23 20:12:56 -0600

    adding back log in function

--- controllers/postController.js
+++ controllers/postController.js
@@ -36,20 +36,20 @@ async function logIn (req,res){
   if(user[0].length === 0){
     return res.render('log-in',{m : "User name or password is not correct"})
   }
-  // if(user){
-  //   const hash = user[0][0].password
-  //   const isUser = await bcrypt.compare(password,hash);
-  //   if(!isUser){
-  //      res.render('404',{m: "Please check your information and try again"})
-  //   }
+  if(user){
+    const hash = user[0][0].password
+    const isUser = await bcrypt.compare(password,hash);
+    if(!isUser){
+       res.render('404',{m: "Please check your information and try again"})
+    }
 
-  //   res.locals.isAuth = true
-  //   req.session.user = user[0][0]
-  //   req.session.save(function(){
-  //     res.render('user',{user: user[0][0]})
-  //   })
+    res.locals.isAuth = true
+    req.session.user = user[0][0]
+    req.session.save(function(){
+      res.render('user',{user: user[0][0]})
+    })
    
-  // }
+  }
 }
 
 module.exports = {
